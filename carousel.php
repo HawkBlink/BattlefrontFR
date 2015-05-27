75727e2b39fa4859a0173d3db0597211d74364c1
HawkBlink <mario.pilloni@outlook.fr>
2015-05-27 22:32:42 +0200

    Version de présentation de l'index du site

---
+++ carousel.php
@@ -0,0 +1,58 @@
+
+<!-- Carousel : les images doivent avoir une taille de 1500x500px
+===========================================================================-->
+<div id="myCarousel" class="carousel slide">
+    <!-- Indication -->
+    <ol class="carousel-indicators">
+        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+        <li data-target="#myCarousel" data-slide-to="1"></li>
+        <li data-target="#myCarousel" data-slide-to="2"></li>
+    </ol>
+    <div class="carousel-inner">
+        <!-- image 1-->
+        <div class="item active">
+            <!-- Image 1-->
+            <img src="css/images/banniere.jpg" style="width: 100%" class="img-responsive">
+            <!-- Image 1-->
+            <div class="container">
+
+                <div class="carousel-caption">
+                    <!-- HTML-->
+                    <h1>vous voulez savoir plus ?</h1>
+                    <p><a class="btn btn-lg btn-danger" href="presentation.php">Présentation</a></p>
+                    <!-- HTML -->
+                </div>
+            </div>
+        </div>
+        <!-- image 2-->
+        <div class="item">
+            <!-- Image 2-->
+            <img src="css/images/url.jpg" style="width: 100%" class="img-responsive">
+            <!-- Image 2-->
+            <div class="container">
+                <div class="carousel-caption">
+
+                </div>
+            </div>
+        </div>
+        <!-- image 3-->
+        <div class="item">
+            <!-- Image 3-->
+            <img src="css/images/url2.jpg" style="width: 100%" class="img-responsive">
+            <!-- Image 3-->
+            <div class="container">
+                <div class="carousel-caption">
+
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- côté -->
+    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+        <span class="icon-prev"></span>
+    </a>
+    <a class="right carousel-control" href="#myCarousel" data-slide="next">
+        <span class="icon-next"></span>
+    </a>
+    <!-- côté -->
+</div>
---
+++ menu.php
@@ -0,0 +1,37 @@
+
+<nav class="navbar navbar-default navbar-fixed-top">
+    <div class="container-fluid">
+        <!-- Adaptation pour les mobiles -->
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                <span class="sr-only">Barre de navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" rel="home" href="#" title="Securoute Maache"><img style="max-width: 70px; margin-top: -7px;" src="css/images/logo_secu.png"></a>
+        </div>
+        <!-- Adaptation pour les mobiles -->
+        <!-- navbar avec un dropdown à modifier -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav">
+                <li><a class="fa fa-home" href="index.php"> Accueil</a></li>
+                <li class="border"><a class="fa fa-th-list" href="tableau_form.php"> Nos Adhérents</a></li>
+                <!-- Début du dropdown -->
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><span class="fa fa-align-left"></span> Des explications ?<span class="caret"></span></a>
+                    <ul class="dropdown-menu" role="menu" >
+                        <li><a href="presentation.php">Présentation</a></li>
+                        <li><a href="#">On met quoi là ?</a></li>
+                        <li><a href="#">Voilà</a></li>
+                        <li class="divider"></li>
+                        <li><a href="#">toujours</a></li>
+                        <li class="divider"></li>
+                        <li><a href="#">Autre chose</a></li>
+                    </ul>
+                </li>
+            </ul>
+
+        </div><!-- /.navbar-collapse -->
+    </div><!-- /.container-fluid -->
+</nav>
